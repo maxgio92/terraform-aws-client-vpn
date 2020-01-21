7de6c8345de52f729098503cbe51eb86f1bcaf8e
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2020-01-21 17:44:24 +0100

    Output target network associations SGs

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,7 @@ output "auth_client_certificate" {
   value     = tls_locally_signed_cert.client.cert_pem
 }
 
+output "target_network_associations_security_groups" {
+  type  = list
+  value = aws_ec2_client_vpn_network_association.default.security_groups
+}
